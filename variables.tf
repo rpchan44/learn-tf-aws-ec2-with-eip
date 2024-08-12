3cf6b14053cac25d218f43b386f90caf26085c4d
Ronaldo Chan <ronaldopchan@outlook.com>
2024-08-12 10:43:29 +0800

    Update variables.tf
    Add a few variables

--- variables.tf
+++ variables.tf
@@ -3,7 +3,16 @@ variable "region" {
   type        = string
   default     = "ap-southeast-1"
 }
-
+variable "ubuntu-ami" { 
+  description = "Ubuntu AMI for ap-southeast-1a"
+  type        = string
+  default     = "ami-060e277c0d4cce553"
+}
+variable "ami_availability_zone" { 
+  description = "Availability Zone"
+  type        = string
+  default     = "ap-southeast-1a"
+}
 variable "instance_type" {
   description = "The type of EC2 instance"
   type        = string
